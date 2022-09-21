402d894b58f5cfe5e5c2a00c154d17ec8e4eb219
unknown <giulanopim98@gmail.comm>
2022-09-20 22:34:15 -0300

    modulo-dos-javascript

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hola')
+const nombre = 'Giuliano'
+
+const edad = 24
+
+const soyDesarrolador = false
+
+const nacimiento = new Date (1998, 04, 01)
+
+const peliculaFavorita = {
+    nombre :'Batman inicia',
+    director: 'Christopher Nolan',
+    estreno: [10,06,2005],
+    duracion: '140 minutos'
+}
+
+console.log(nombre)
+console.log(edad)
+console.log(soyDesarrolador)
+console.log(nacimiento)
+console.log(peliculaFavorita)
