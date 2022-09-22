66882c8f437ed6d54a2fdf297f3da267729a7484
unknown <giulanopim98@gmail.comm>
2022-09-21 22:24:03 -0300

    bucles

---
+++ factorial-break.js
@@ -0,0 +1,30 @@
+let listas = [1,2,3,4,5,6,7,8,9,10]
+
+for (let i = 0; i< listas.length; i++)
+{
+    if(listas[i]===1){
+        continue;
+    }
+    if(listas[i]===3){
+        continue;
+    }
+    if(listas[i]===4){
+        continue;
+    }
+    if(listas[i]===6){
+        continue;
+    }
+    if(listas[i]===7){
+        continue;
+    }
+    if(listas[i]===8){
+        continue;
+    }
+    if(listas[i]===9){
+        continue;
+    }
+   
+    console.log(listas[i])
+if (listas[i]>10){
+    break;
+}}
