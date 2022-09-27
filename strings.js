455e0bcfb9c9981e5a169fa9f73f78ceb798622f
unknown <giulanopim98@gmail.comm>
2022-09-26 22:56:51 -0300

    strings-js-2

---
+++ strings.js
@@ -0,0 +1,14 @@
+const nombre = 'Giuliano.'
+const apellido = 'Pimenta.'
+const estudiante = `${nombre}  ${apellido}`
+console.log (estudiante.toLowerCase())
+console.log (estudiante.toUpperCase())
+console.log(estudiante.length)
+console.log(nombre.charAt(0))
+console.log(apellido.charAt(0))
+console.log(estudiante.trim().length)
+console.log(estudiante.includes(`${nombre}`))
+
+
+
+
