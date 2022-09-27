fc2fc3833d745a9fb743c374f744578bc0817268
unknown <giulanopim98@gmail.comm>
2022-09-27 20:00:33 -0300

    numbers

---
+++ numbers.js
@@ -0,0 +1,6 @@
+const alturaCM = 175
+const alturaMetros = 1.75
+const peso = 85.1
+const altura = alturaMetros.toPrecision(3)
+const alturaMin = alturaMetros.toFixed(3)
+const js =  'el máximo valor que se puede obtener en Javascript + 1'
