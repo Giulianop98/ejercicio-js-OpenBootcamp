689bce3aff4b4f25b779761f8ed57990b256183e
unknown <giulanopim98@gmail.comm>
2022-10-03 23:06:41 -0300

    fechas-ejercicios

--- sets.js
+++ sets.js
@@ -17,3 +17,14 @@ const prop= 'nombre'
 
 console.log(obj[prop])
 
+//fechas
+console.log(new Date())
+console.log(new Date(1998,4,1,6,30))
+const fecha = new Date()
+const fecha2= new Date(1998, 4, 1, 8, 30, 20, 160)
+const fechas= fecha>fecha2
+const mes= fecha2.getMonth()
+const año= fecha2.getFullYear()
+
+
+
