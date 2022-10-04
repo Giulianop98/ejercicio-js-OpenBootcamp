338c9006e7b77c444e2b9129ead7da954c41982e
unknown <giulanopim98@gmail.comm>
2022-10-03 22:59:01 -0300

    objetos-ejercicios

--- sets.js
+++ sets.js
@@ -3,3 +3,17 @@ const lista = ['hola', 'chau', 'bueno', 'hola']
 const listaSet= new Set (lista)
 
 listaSet.add('javascript')
+
+//objetos
+
+const obj={
+    nombre:'Giuliano',
+    apellido:'Pimenta',
+    edad:24,
+    isDeveloper: false,
+}
+
+const prop= 'nombre'
+
+console.log(obj[prop])
+
